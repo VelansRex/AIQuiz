e45d201eafc260b854168c2bb5e56d4c528271ab
Paweł Pikulik <velans@tlen.pl>
2025-07-04 15:17:17 +0200

    The app uses Microsoft Azure OpenAI API and GPT-4o-mini model.

--- AIQuiz.py
+++ AIQuiz.py
@@ -1,18 +1,6 @@
 
-"""
-Name: AI Quiz
-Description: Azure OpenAI-Powered Quiz
-
-Instruction:
-1. Download AI Quiz Git repository: git clone https://github.com/VelansRex/AIQuiz.git
-2. Install openai and python-dotenv libraries: pip install -r requirements.txt
-2. Run the app with command: python aiquiz.py
-
-About App:
-The app uses Microsoft Azure OpenAI API and GPT-4 Turbo model.
-"""
-
 import os
+import sys
 from dotenv import load_dotenv
 from openai import AzureOpenAI
 
@@ -26,17 +14,16 @@ client = AzureOpenAI(
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 )
 
-
 deployment_name = os.getenv("DEPLOYMENT_NAME")
-
 # Loading a Text File
-def read_text_from_file(filename):
+def resource_path(relative_path):
+    # Path to resource for PyInstaller
     try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            return file.read()
-    except FileNotFoundError:
-        print(f"Error: {filename} not found.")
-        return ""
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 # System Prompt
 system_prompt = """
@@ -83,10 +70,24 @@ def generate_streamed_question(text):
 
 # Application Section
 if __name__ == "__main__":
-    content = read_text_from_file("ArtificialIntelligenceExamples.txt")
+    content = resource_path("ArtificialIntelligenceExamples.txt")
+
+    print("Name: AI Quiz \nDescription: Azure OpenAI-Powered Quiz")
+    print("About: The app uses Microsoft Azure OpenAI API and GPT-4o-mini model.")
+    print("Instruction:")
+    print("\
+        1. Download AI Quiz: https://storageazureother.blob.core.windows.net/aiquiz/AIQuiz.exe\n\
+        2. Run File: AIQuiz.exe\n\
+        3. Project Repo: https://github.com/VelansRex/AIQuiz.git\n\
+        ")
+    print("--------------------")
 
     if content:
         print("\nQuiz Question:\n")
         for i in range(5):
             print(f"\nQuestion {i+1}:")
             generate_streamed_question(content)
+
+    print()
+    input("Press Enter to exit...")
+
